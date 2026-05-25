7345b08b2839ef0d85a186ed3ecf5dbc9aec956d
Rabedrafitra <166728136+rabedrafitra@users.noreply.github.com>
2026-05-25 10:44:03 +0300

    Wrap SignIn component with Wrapper componentsin

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,14 @@
+import Wrapper from '@/app/components/Wrapper'
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
     return (
+        <Wrapper>
         <div className='flex justify-center items-center  h-screen'>
             <SignIn />
         </div>
+        </Wrapper>
     )
 }
 
